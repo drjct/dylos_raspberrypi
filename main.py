fd3885af0144b58b339c319d2b4c7b5a341668c6
jctan05 <38368029+jctan05@users.noreply.github.com>
2018-08-25 23:09:58 -0700

    Cleaned main.py

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import serial
 from ubidots import ApiClient
 
 #Dylos Serial Port Setup
-#https://www.sylvane.com/media/documents/products/dc1100-com-port-option.pdf
+#Documentation link: https://www.sylvane.com/media/documents/products/dc1100-com-port-option.pdf
+
+#Set PySerial port settings according to documentation requirements
 ser = serial.Serial(
         port='/dev/ttyUSB0',
         baudrate = 9600,
@@ -13,16 +15,15 @@ ser = serial.Serial(
 
 ser.flushInput() #buffer input to make sure everything is cleared before uploading
 
-print(ser) #diagnostic, make sure everything works
-
+#print(ser) #diagnostic, make sure everything works
 
 #The serial output format is small counts, comma, large counts, carriage return, line feed:
 #Example:
 #675,19<CR><LF>
 #Units are particles/.01 cubic foot
-#A reading of 2 means 200 particles per cubic foot
+#For example, a reading of 2 means 200 particles per cubic foot
 
-
+#Extract serial output from Dylos
 while True:
     data = ser.readline().decode('utf-8')
     small, large = data.split(',') #place the outputs into separate variables
@@ -30,14 +31,15 @@ while True:
     large = float(large)
 
     #https://raspi-temp-rep.readthedocs.io/en/master/ubidots/#ubidots-python-api-client
+
     ######Create an "API" object
     api = ApiClient("A1E-3f71cbb44ff5809eca0c202dbac40221cf44") #API Key goes here
     ######Create a "Variable" object
     small_var = api.get_variable("5b80d8b5c03f9755676f791b")
     large_var = api.get_variable("5b7e25f5c03f973ef812404f") #Variable API ID goes here
-    ######Here is where you usually put the code to capture the data, either through your GPIO pins or as a calculation. We'll simply put a random value here:
+    ######Calculation of PPM 2.5 and PPM 10
     #PPM 2.5 = (small - large)/100 micrograms per cubic meters
-    #PPM 10 = (large/3) + PM2.5 (micrograms per cubic meters)
+    #PPM 10 = (large/3) + PM2.5 micrograms per cubic meters
     small_calc = (small - large)/100
     large_calc = (large/3) + small_calc
 
